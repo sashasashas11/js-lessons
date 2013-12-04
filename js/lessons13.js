6d895a2b0d52aba96947b9377aae23d351225081
Sasha <dimurchik@ukr.net>
2013-12-04 22:55:09 +0200

    xml

--- js/lessons13.js
+++ js/lessons13.js
@@ -1,11 +1,12 @@
 /**
  * Created by sasha on 28.11.13.
  */
-$(function(){
+(function(){
+	var newDate, theDate;
+	theDate = new Date(2000, 4, 13);
+	console.log(theDate);
+	newDate = theDate.setDate(theDate.getDate()+465);
+//	rrr = new Date(newDate);
+	console.log(new Date(newDate));
 
-	var obj = {
-		field: 1,
-		type: "asd",
-		func: function(){}
-	}
-});
+})();
